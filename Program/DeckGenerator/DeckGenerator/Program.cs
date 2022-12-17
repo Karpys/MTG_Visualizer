a7cbcd28216e0264e0fab775e342022d564a31b6
Rémi Vedrenne <remi.vedrenne@dualcat.io>
2022-12-17 01:28:24 +0100

    [CODE] Add deck generator

---
+++ Program/DeckGenerator/DeckGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using DeckGenerator;
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        DeckCreator generator = new DeckCreator();
+        generator.GenerateDeck();
+    }
+    
+}
