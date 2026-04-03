7c6cf541fa63b313335732e314b129738aca8053
Rémi Vedrenne <remi.vedrenne.pro@gmail.com>
2026-04-03 10:52:26 +0200

    [CODE] Add mana cost filter code

---
+++ Assets/Script/ApiRequester/ManaCostFilter.cs
@@ -0,0 +1,121 @@
+namespace Script
+{
+    using System;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class ManaCostFilter : MonoBehaviour
+    {
+        [SerializeField] private string m_Request = String.Empty;
+        [SerializeField] private ManaSymbolSearchType m_ManaSearchType = ManaSymbolSearchType.Contains;
+        private List<ManaSymbol> m_ManaSymbols = new List<ManaSymbol>();
+        
+        public void AddSymbol(string symbol)
+        {
+            switch (symbol)
+            {
+                case "1":
+                    m_ManaSymbols.Add(ManaSymbol.Colorless);
+                    break;
+                case "W":
+                    m_ManaSymbols.Add(ManaSymbol.White);
+                    break;
+                case "B":
+                    m_ManaSymbols.Add(ManaSymbol.Black);
+                    break;
+                case "U":
+                    m_ManaSymbols.Add(ManaSymbol.Blue);
+                    break;
+                case "G":
+                    m_ManaSymbols.Add(ManaSymbol.Green);
+                    break;
+                case "R":
+                    m_ManaSymbols.Add(ManaSymbol.Red);
+                    break;
+            }
+        }
+        
+        public void RemoveSymbol(string symbol)
+        {
+            switch (symbol)
+            {
+                case "1":
+                    m_ManaSymbols.Remove(ManaSymbol.Colorless);
+                    break;
+                case "W":
+                    m_ManaSymbols.Remove(ManaSymbol.White);
+                    break;
+                case "B":
+                    m_ManaSymbols.Remove(ManaSymbol.Black);
+                    break;
+                case "U":
+                    m_ManaSymbols.Remove(ManaSymbol.Blue);
+                    break;
+                case "G":
+                    m_ManaSymbols.Remove(ManaSymbol.Green);
+                    break;
+                case "R":
+                    m_ManaSymbols.Remove(ManaSymbol.Red);
+                    break;
+            }
+        }
+
+        private string ComputeRequest()
+        {
+            return m_Request;
+            int colorlessCount = 0;
+            string request = "";
+
+            for (int i = 0; i < m_ManaSymbols.Count; i++)
+            {
+                switch (m_ManaSymbols[i])
+                {
+                    case ManaSymbol.Red:
+                        request += "R";
+                        break;
+                    case ManaSymbol.Green:
+                        request += "G";
+                        break;
+                    case ManaSymbol.Blue:
+                        request += "U";
+                        break;
+                    case ManaSymbol.White:
+                        request += "W";
+                        break;
+                    case ManaSymbol.Black:
+                        request += "B";
+                        break;
+                    case ManaSymbol.Colorless:
+                        colorlessCount += 1;
+                        break;
+                }
+            }
+
+            if (colorlessCount > 0)
+                request += colorlessCount;
+
+            return request;
+        }
+        
+        public string GetFilter()
+        {
+            if (ComputeRequest() == "")
+                return "";
+            
+            return "+m" + ComputeSearchType() +  ComputeRequest();
+        }
+
+        private string ComputeSearchType()
+        {
+            switch (m_ManaSearchType)
+            {
+                case ManaSymbolSearchType.Same:
+                    return "=";
+                case ManaSymbolSearchType.Contains:
+                    return ":";
+            }
+
+            return ":";
+        }
+    }
+}
